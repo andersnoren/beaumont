a26c841ae812f419ab576647c66a2384a3d8d384
Anders <anders@andersnoren.se>
2022-09-08 16:40:43 +0200

    Progress!

--- functions.php
+++ functions.php
@@ -4,26 +4,26 @@
 	THEME SUPPORTS
 --------------------------------------------------------------------------------------------------- */
 
-function bjork_setup() {
+function king_setup() {
 
 	add_theme_support( 'wp-block-styles' );
 
 	add_editor_style( 'style.css' );
 
 }
-add_action( 'after_setup_theme', 'bjork_setup' );
+add_action( 'after_setup_theme', 'king_setup' );
 
 
 /*	-----------------------------------------------------------------------------------------------
 	ENQUEUE STYLESHEETS
 --------------------------------------------------------------------------------------------------- */
 
-function bjork_styles() {
+function king_styles() {
 
-	wp_enqueue_style( 'bjork-styles', get_theme_file_uri( '/style.css' ), array(), wp_get_theme( 'bjork' )->get( 'Version' ) );
+	wp_enqueue_style( 'king-styles', get_theme_file_uri( '/style.css' ), array(), wp_get_theme( 'king' )->get( 'Version' ) );
 
 }
-add_action( 'wp_enqueue_scripts', 'bjork_styles' );
+add_action( 'wp_enqueue_scripts', 'king_styles' );
 
 
 /*	-----------------------------------------------------------------------------------------------
@@ -31,35 +31,35 @@ add_action( 'wp_enqueue_scripts', 'bjork_styles' );
 	Register theme specific block pattern categories. The patterns themselves are stored in /patterns/.
 --------------------------------------------------------------------------------------------------- */
 
-function bjork_register_block_patterns() {
+function king_register_block_patterns() {
 
 	if ( ! function_exists( 'register_block_pattern_category' ) ) return;
 
-	// The block pattern categories included in Björk.
-	$bjork_block_pattern_categories = apply_filters( 'bjork_block_pattern_categories', array(
-		'bjork-blog'  => array(
-			'label'			=> esc_html__( 'Björk Blog', 'bjork' ),
+	// The block pattern categories included in King.
+	$king_block_pattern_categories = apply_filters( 'king_block_pattern_categories', array(
+		'king-blog'  => array(
+			'label'			=> esc_html__( 'King Blog', 'king' ),
 		),
-		'bjork-cta'  => array(
-			'label'			=> esc_html__( 'Björk Call to Action', 'bjork' ),
+		'king-cta'  => array(
+			'label'			=> esc_html__( 'King Call to Action', 'king' ),
 		),
-		'bjork-general' => array(
-			'label'			=> esc_html__( 'Björk General', 'bjork' ),
+		'king-general' => array(
+			'label'			=> esc_html__( 'King General', 'king' ),
 		),
 	) );
 
 	// Sort the block pattern categories alphabetically based on the label value, to ensure alphabetized order when the strings are localized.
-	uasort( $bjork_block_pattern_categories, function( $a, $b ) { 
+	uasort( $king_block_pattern_categories, function( $a, $b ) { 
 		return strcmp( $a["label"], $b["label"] ); }
 	);
 
 	// Register block pattern categories.
-	foreach ( $bjork_block_pattern_categories as $slug => $settings ) {
+	foreach ( $king_block_pattern_categories as $slug => $settings ) {
 		register_block_pattern_category( $slug, $settings );
 	}
 
 }
-add_action( 'init', 'bjork_register_block_patterns' );
+add_action( 'init', 'king_register_block_patterns' );
 
 
 /*	-----------------------------------------------------------------------------------------------
@@ -67,23 +67,23 @@ add_action( 'init', 'bjork_register_block_patterns' );
 	Register theme specific block styles.
 --------------------------------------------------------------------------------------------------- */
 
-if ( ! function_exists( 'bjork_register_block_styles' ) ) :
-	function bjork_register_block_styles() {
+if ( ! function_exists( 'king_register_block_styles' ) ) :
+	function king_register_block_styles() {
 
 		if ( ! function_exists( 'register_block_style' ) ) return;
 
 		// Separator: Angled Separator
 		register_block_style( 'core/separator', array(
-			'name'  	=> 'bjork-angled-separator',
-			'label' 	=> esc_html__( 'Angled', 'bjork' ),
+			'name'  	=> 'king-angled-separator',
+			'label' 	=> esc_html__( 'Angled', 'king' ),
 		) );
 
 		// Separator: Angled Separator Wide
 		register_block_style( 'core/separator', array(
-			'name'  	=> 'bjork-angled-separator-wide',
-			'label' 	=> esc_html__( 'Angled Wide', 'bjork' ),
+			'name'  	=> 'king-angled-separator-wide',
+			'label' 	=> esc_html__( 'Angled Wide', 'king' ),
 		) );
 		
 	}
-	add_action( 'init', 'bjork_register_block_styles' );
+	add_action( 'init', 'king_register_block_styles' );
 endif;
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-=== Björk ===
+=== King ===
 Contributors: Anlino
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=anders%40andersnoren%2ese&lc=US&item_name=Free%20WordPress%20Themes%20from%20Anders%20Noren&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
 Requires at least: 6.0
@@ -18,26 +18,21 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 == Licenses ==
 
-Björk is derived from the Davis Blocks theme, Copyright (c) 2022 Anders Norén.
+King is derived from the Davis Blocks theme, Copyright (c) 2022 Anders Norén.
 Davis Blocks is distributed under the terms of the GNU GPL version 2.0.
 
-Björk bundles the following third-party resources:
+King bundles the following third-party resources:
 
 Albert Sans font
 License: SIL Open Font License, 1.1, https://opensource.org/licenses/OFL-1.1
-Source: https://fonts.google.com/specimen/Albert+Sans/
+Source: https://fonts.google.com/specimen/Albert+Sans
 
-All illustrations in the theme files and screenshot.png by Anders Norén
-License: Creative Commons Zero, https://creativecommons.org/publicdomain/zero/1.0/
+STIX Two Text font
+License: SIL Open Font License, 1.1, https://opensource.org/licenses/OFL-1.1
+Source: https://fonts.google.com/specimen/STIX+Two+Text
 
 
 == Changelog ==
 
-Version 0.1.1 (2022-09-07)
--------------------------
-- Changed the loop.html template part to use the blog-grid-cols-2.php pattern by default, matching the demo site.
-- Fixed the height of the sidebar being incorrect when the user admin bar is displayed (thanks, @JeroenRotty).
-- Adjusted the theme description.
-
-Version 0.1 (2022-09-02)
+Version 0.1 (2022-XX-XX)
 -------------------------
