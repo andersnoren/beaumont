96a86acaaf2ce71b6a6478156288958dadcf210c
Anders <anders@andersnoren.se>
2022-09-12 15:04:58 +0200

    Large pull quote pattern

---
+++ patterns/pullquote-large.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Large pullquote.
+ * Slug: king/pullquote-large
+ * Categories: king
+ */
+?>
+<!-- wp:cover {"overlayColor":"foreground","minHeight":50,"minHeightUnit":"vh","align":"full"} -->
+<div class="wp-block-cover alignfull" style="min-height:50vh"><span aria-hidden="true"
+		class="wp-block-cover__background has-foreground-background-color has-background-dim-100 has-background-dim"></span>
+	<div class="wp-block-cover__inner-container">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"0px","margin":{"top":"0px","bottom":"0px"}}}} -->
+		<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px">
+			<!-- wp:pullquote {"style":{"typography":{"letterSpacing":"-0.01em","fontStyle":"normal","fontWeight":"400"}},"fontSize":"huge"} -->
+			<figure class="wp-block-pullquote has-huge-font-size"
+				style="font-style:normal;font-weight:400;letter-spacing:-0.01em">
+				<blockquote>
+					<p><em>”That changed my life as an artist. It was the most stirring thing that I’ve ever seen.”</em></p>
+				</blockquote>
+			</figure>
+			<!-- /wp:pullquote -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+</div>
+<!-- /wp:cover -->
